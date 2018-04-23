55805f4284fe5820bb19a4a382d286405f312955
Deauveide <deauveide@javerianacali.edu.co>
2018-04-22 19:18:58 -0500

    Sin llamar a funciones

--- testSuma.py
+++ testSuma.py
@@ -1,10 +1,7 @@
-def suma(a, b):
+def suma():
     """
     Funcion que suma las entradas a y b y da su salida por consola
     """
-    return a + b
-
-
-num1 = int(input("Ingrese primer numero"))
-num2 = int(input("Ingrese segundo numero"))
-print("La suma de", num1, "y", num2, "es", suma(num1, num2))
+    num1 = int(input("Ingrese primer numero"))
+    num2 = int(input("Ingrese segundo numero"))
+    return "La suma de", num1, "y", num2, "es", num1 + num2
