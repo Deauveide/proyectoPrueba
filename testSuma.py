bddb00d9041788fe3b4ccc2179f6fb400498b6a1
Deauveide <deauveide@javerianacali.edu.co>
2018-04-22 19:05:03 -0500

    Programa que suma dos numeros

---
+++ testSuma.py
@@ -0,0 +1,9 @@
+def suma(a, b):
+    """
+    Funcion que suma las entradas a y b y da su salida por consola
+    """
+    c = a + b
+    return c
+num1 = int(input("Ingrese primer numero"))
+num2 = int(input("Ingrese segundo numero"))
+print()
